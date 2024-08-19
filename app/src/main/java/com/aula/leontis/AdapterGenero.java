1bb9f78618c322fc1f671ba58382e3dc5310d0e9
SamiraSouza07 <samira.campos@germinare.org.br>
2024-08-19 08:18:33 -0300

    Adicionanado a tela de interesses com o recycler view de generos

---
+++ app/src/main/java/com/aula/leontis/AdapterGenero.java
@@ -0,0 +1,113 @@
+package com.aula.leontis;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+import static com.google.android.material.resources.MaterialResources.getDrawable;
+
+import android.app.Dialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdapterGenero extends RecyclerView.Adapter<AdapterGenero.viewHolderGenero>{
+    private List<Genero> listaGeneros;
+    public AdapterGenero(List<Genero> listaGeneros){
+        this.listaGeneros = listaGeneros;
+    }
+    @NonNull
+    @Override
+    public viewHolderGenero onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View viewItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_genero,parent,false);
+        return new viewHolderGenero(viewItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolderGenero holder, int position) {
+        holder.nomeGenero.setText(listaGeneros.get(holder.getAdapterPosition()).getNome());
+
+        if (listaGeneros.get(holder.getAdapterPosition()).getCheckInteresse()){
+            holder.checkInteresse.setChecked(true);
+        }else{
+            holder.checkInteresse.setChecked(false);
+        }
+
+        holder.checkInteresse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(holder.checkInteresse.isChecked()){
+                    listaGeneros.get(holder.getAdapterPosition()).setCheckInteresse(true);
+                }else{
+                    listaGeneros.get(holder.getAdapterPosition()).setCheckInteresse(false);
+                }
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                String teste ="";
+                teste += listaGeneros.get(holder.getAdapterPosition()).getNome() +" " + listaGeneros.get(holder.getAdapterPosition()).getCheckInteresse();
+                Toast.makeText(holder.itemView.getContext(), teste, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        holder.btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(holder.itemView.getContext());
+                dialog.setContentView(R.layout.caixa_mensagem);
+                dialog.getWindow().setLayout(WRAP_CONTENT,WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawableResource(R.drawable.caixa_mensagem_fundo);
+                dialog.setCancelable(false);
+
+                TextView titulo = dialog.findViewById(R.id.titulo);
+                TextView introducao = dialog.findViewById(R.id.mensagem);
+                titulo.setText(listaGeneros.get(holder.getAdapterPosition()).getNome());
+                introducao.setText(listaGeneros.get(holder.getAdapterPosition()).getIntroducao());
+
+                Button btnFechar = dialog.findViewById(R.id.btn_fechar);
+                btnFechar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaGeneros.size();
+    }
+
+    public class viewHolderGenero extends RecyclerView.ViewHolder{
+        TextView nomeGenero;
+        CheckBox checkInteresse;
+        ImageButton btnInfo;
+
+
+    public viewHolderGenero(@NonNull View itemView) {
+        super(itemView);
+        nomeGenero = itemView.findViewById(R.id.titulo_genero);
+        checkInteresse = itemView.findViewById(R.id.check_interesse);
+        btnInfo = itemView.findViewById(R.id.btn_info);
+
+    }
+}
+}
--- app/src/main/java/com/aula/leontis/TelaCadastroInteresses.java
+++ app/src/main/java/com/aula/leontis/TelaCadastroInteresses.java
@@ -1,16 +1,35 @@
 package com.aula.leontis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TelaCadastroInteresses extends AppCompatActivity {
+    RecyclerView rvGeneros;
+    List<Genero> listaGeneros = new ArrayList<>();
+    AdapterGenero adapterGenero = new AdapterGenero(listaGeneros);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_cadastro_interesses);
+        rvGeneros = findViewById(R.id.rv_generos);
+        rvGeneros.setAdapter(adapterGenero);
+
+        listaGeneros.add(new Genero(1,"Ação", true,"Ação e aventura."));
+        listaGeneros.add(new Genero(2,"Aventura", false,"Aventuraaaaa."));
+        listaGeneros.add(new Genero(3,"Comedia", false,"Comediaaaaa."));
+        listaGeneros.add(new Genero(4,"Drama", false,"Dramaaaaa."));
+        listaGeneros.add(new Genero(5,"Ficcao Cientifica", false,"Ficcao Cientificaaaaaa."));
+        listaGeneros.add(new Genero(6,"Romance", false,"Romanceeeeee."));
+
+        rvGeneros.setLayoutManager(new androidx.recyclerview.widget.LinearLayoutManager(this));
+
 
         //pegando informações de cadastro das telas anteriores
         Intent infoCadastro = getIntent();
@@ -23,6 +42,8 @@ public class TelaCadastroInteresses extends AppCompatActivity {
         String apelido = infoCadastro.getStringExtra("apelido");
         String biografia = infoCadastro.getStringExtra("biografia");
         String sexo = infoCadastro.getStringExtra("sexo");
+
+
 
 
     }
